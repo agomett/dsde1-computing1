77276640b72f529b17f5503686c19747825387b3
agomett <tl2219@ic.ac.uk>
2019-11-11 16:48:46 +0000

    keywords changes

--- Week-4/keywords.py
+++ Week-4/keywords.py
@@ -15,6 +15,15 @@ with default values.
 # if both user_name and place are provided
 # it returns 'Hello, <user_name>, and welcome to <place>
 
+def welcome_message(user_name="", place=""):
+    if user_name == "" and place == "":
+        return 'Hello and welcome'
+    if user_name != "" and place == "":
+        return 'Hello, {}, and welcome'.format(user_name)
+    if place != "" and user_name == "":
+        return 'Hello and welcome to {}'.format(place)
+    else:
+        return 'Hello, {}, and welcome to {}'.format(user_name, place)
 
 # Create a function called list_average()
 # without using any libraries to do the maths for you 
@@ -27,3 +36,11 @@ with default values.
 # (return list of all modes if there is a tie between multiple values)
 # if avg_type='mean', return the mean of the list
 # if avg_type='median', return the median of this list
+
+def list_average(input_list, avg_type = "mean"):
+    if avg_type=='mode':
+        return max(input_list, key = input_list.count)
+    if avg_type=='mean':
+        return sum(input_list)/len(input_list) 
+    if avg_type=='median':
+        return sorted(input_list) [len(input_list)//2]
